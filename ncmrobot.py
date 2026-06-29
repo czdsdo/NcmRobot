a8de4533ad82311f6b44712043d32df9b527719e
陈序 <czdyxs@qq.com>
2026-06-29 14:52:43 +0800

    Add Windows ncm conversion support

--- ncmrobot.py
+++ ncmrobot.py
@@ -5,6 +5,45 @@ import os, sys, time
 from os.path import expanduser
 import ntpath
 import subprocess
+import ctypes
+
+
+FO_DELETE = 0x0003
+FOF_ALLOWUNDO = 0x0040
+FOF_NOCONFIRMATION = 0x0010
+FOF_SILENT = 0x0004
+
+
+class SHFILEOPSTRUCTW(ctypes.Structure):
+    _fields_ = [
+        ("hwnd", ctypes.c_void_p),
+        ("wFunc", ctypes.c_uint),
+        ("pFrom", ctypes.c_wchar_p),
+        ("pTo", ctypes.c_wchar_p),
+        ("fFlags", ctypes.c_ushort),
+        ("fAnyOperationsAborted", ctypes.c_bool),
+        ("hNameMappings", ctypes.c_void_p),
+        ("lpszProgressTitle", ctypes.c_wchar_p),
+    ]
+
+
+def send_to_recycle_bin(path):
+    if os.name != "nt":
+        raise OSError("Recycle bin support is only implemented for Windows")
+
+    operation = SHFILEOPSTRUCTW()
+    operation.wFunc = FO_DELETE
+    operation.pFrom = path + "\0\0"
+    operation.fFlags = FOF_ALLOWUNDO | FOF_NOCONFIRMATION | FOF_SILENT
+
+    result = ctypes.windll.shell32.SHFileOperationW(ctypes.byref(operation))
+    if result != 0:
+        raise OSError(f"Failed to recycle file {path}, code={result}")
+
+if hasattr(sys.stdout, "reconfigure"):
+    sys.stdout.reconfigure(encoding="utf-8", errors="replace")
+if hasattr(sys.stderr, "reconfigure"):
+    sys.stderr.reconfigure(encoding="utf-8", errors="replace")
 
 home = expanduser("~")
 
@@ -18,17 +57,22 @@ CONFIG_KEY_LAST_TS = "last_timestamp"
 if os.path.isfile(configFilePath):
     print("The user config file existing!! Use it.")
 else:
-    config = yaml.safe_load(open(configTempFilePath))
+    with open(configTempFilePath, encoding='utf-8') as infile:
+        config = yaml.safe_load(infile)
 
     userPathIni = home + "/Music/网易云音乐"
     config[CONFIG_KEY_FOLDER] = userPathIni
     with open(configFilePath, 'w', encoding='utf-8') as outfile:
         yaml.dump(config, outfile, default_flow_style=False, allow_unicode=True)
 
-config = yaml.safe_load(open(configFilePath))
+with open(configFilePath, encoding='utf-8') as infile:
+    config = yaml.safe_load(infile)
 
 dirPath = config.get(CONFIG_KEY_FOLDER)
 print(dirPath)
+
+if not os.path.isdir(dirPath):
+    raise FileNotFoundError("Music folder does not exist: " + dirPath)
 
 # get all entries in the directory w/ stats
 entries = (os.path.join(dirPath, fn) for fn in os.listdir(dirPath) if fn.endswith(".ncm"))
@@ -43,19 +87,34 @@ entries = ((stat[ST_CTIME], path)
 lastTS = config[CONFIG_KEY_LAST_TS]
 newLastTS = lastTS
 sumHandle = 0
+sumRecycled = 0
+dump_executable = "./ncmdump.exe" if os.name == "nt" else "./ncmdump"
 for cdate, path in sorted(entries):
+    mp3_path = os.path.splitext(path)[0] + ".mp3"
+
+    if os.path.isfile(mp3_path):
+        print("### Recycle source file because mp3 exists:", ntpath.basename(path))
+        send_to_recycle_bin(path)
+        sumRecycled = sumRecycled + 1
+        continue
+
     # print(cdate)
     # print(time.ctime(cdate), os.path.basename(path), os.name)
     if cdate > lastTS:
         fileToConvert = ntpath.basename(path)
         print("### Start to convert the file: ", cdate, "-", fileToConvert)
-        subprocess.run(["./ncmdump", path])
+        subprocess.run([dump_executable, path], check=True)
         sumHandle = sumHandle + 1
+        if os.path.isfile(mp3_path):
+            print("### Recycle source file after conversion:", fileToConvert)
+            send_to_recycle_bin(path)
+            sumRecycled = sumRecycled + 1
     if cdate > newLastTS:
         newLastTS = cdate
 
 print("new last time stamp: ", newLastTS, "time: ", time.ctime(newLastTS))
 print("sum of handle this time: ", sumHandle)
+print("sum of recycled ncm this time: ", sumRecycled)
 
 config[CONFIG_KEY_LAST_TS] = newLastTS
 with open(configFilePath, 'w', encoding='utf-8') as outfile:
